d5ee1759134126993825cc581638c22a43c9c05c
ahava <minoraherinirina72@gmail.com>
2023-03-07 07:18:35 +0100

    3

--- frontservlet.java
+++ frontservlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller;
+package etu1985.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
